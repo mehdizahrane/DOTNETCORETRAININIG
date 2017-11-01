0fed10c70c0a81a789171a05db6b47f662f7b9f9
zahranemehdi <zahranemehdi@gmail.com>
2017-11-01 14:29:24 +0000

    update about page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -27,14 +27,12 @@ namespace CoreTraining.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
